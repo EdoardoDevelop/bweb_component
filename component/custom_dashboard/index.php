7392995cc17e0815224eb89a6ea79ae8512954ca
edoardo.develop@gmail.com <edoardo.develop@gmail.com>
2023-01-30 06:44:59 +0100

    bug fix

---
+++ component/custom_dashboard/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * ID: custom_dashboard
+ * Name: Custom Dashboard
+ * Description: Dashboard personalizzato.
+ * Icon: dashicons-dashboard
+ * 
+ */
+
+ 
+require plugin_dir_path( __FILE__ ) ."inc/settings.php";
