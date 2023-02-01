745739b1d7177056299c32aa1cd400fdcf8c0577
edoardo.develop@gmail.com <edoardo.develop@gmail.com>
2023-02-01 06:55:53 +0100

    custom_dashboard bug fix

--- component/custom_dashboard/assets/script.js
+++ component/custom_dashboard/assets/script.js
@@ -1,123 +1,148 @@
 jQuery(function($){
 
-        var boxwidget = $( ".boxwidget" ).sortable({
-            revert: 100,
-            tolerance: "pointer",
-            stop: function(){
-                $( ".item_widget:not(.ui-sortable-placeholder)" ).removeAttr('style');
-            }
-        });
-        $( ".boxwidget" ).disableSelection();
-        var adminmenu_a = $( "#adminmenu a" ).draggable({
-            connectToSortable: $( ".boxwidget" ),
-            helper: function(e) {
-                var icon = 'dashicons-open-folder';
-                //var classItem = $('.wp-menu-image',this).removeClass('wp-menu-image').removeClass('dashicons-before').attr('class');
-                var classItem = $('.wp-menu-image',this).attr('class');
-                if((classItem != undefined) && (!$('.wp-menu-image',this).hasClass('svg'))){
-                    icon = classItem;
-                }
-                
-                return $('<div class="item_widget"><a href="'+$(this).attr('href')+'" ><span class="box_icons dashicons '+icon+'"></span>'+$(this).text()+'</a></div>');
-            },
-            revert: "invalid",
-            stop: function( event, ui ) {
-                
-                $( ".item_widget" ).draggable("instance");
-                refresh_d(true);
-                
-            }
-            
-        });
-        var add_element_item_widget = $( ".add_element .item_widget" ).draggable({
-            connectToSortable: $( ".boxwidget" ),
-            revert: 'invalid',
-            helper: 'clone',
-            tolerance: "pointer",
-            stop: function( event, ui ) {
-                refresh_d(true);
-            }
-        })
-        
+        var cont_widget;
+        var boxwidget;
+        var adminmenu_a;
+        var add_element_item_widget;
         var boxwidget_item_widget;
-
-        var add_element_item_widget_select = $( ".add_element .item_widget_select" ).draggable({
-            connectToSortable: $( ".boxwidget" ),
-            revert: 'invalid',
-            helper: function(e) {
-                var type = $(this).attr('attr-type');
-                var id = $('#id'+type+' option:selected' ).val();
-                var title = $('#id'+type+' option:selected' ).text();
-                return $('<div class="item_widget"><a href="http://localsite.ddns.net/test/wp-admin/post.php?post='+id+'&action=edit" ><span class="box_icons"></span>'+title+'</a></div></div>');
-            },
-            stop: function( event, ui ) {
-                
-                //$( ".item_widget:not(.ui-sortable-placeholder)" ).removeAttr('style');
-                $( ".item_widget" ).draggable("instance");
-                refresh_d(true);
-            },
-            tolerance: "pointer"
-        })
-
-        var add_element_item_widget_text = $( ".add_element .item_widget_text" ).draggable({
-            connectToSortable: $( ".boxwidget" ),
-            revert: 'invalid',
-            helper: function(e) {
-                var t = $('#input_widget_text').val();
-                
-                return $('<div class="item_widget">'+t+'</div>');
-            },
-            stop: function( event, ui ) {
-                //$( ".item_widget:not(.ui-sortable-placeholder)" ).removeAttr('style');
-                $( ".item_widget" ).draggable("instance");
-                refresh_d(true);
-            },
-            tolerance: "pointer"
-        })
-
-        var trash_dash = $( "#trash_dash" ).droppable({
-            accept: ".boxwidget .item_widget",
-            classes: {
-                "ui-droppable-hover": "trash-state-active"
-            },
-            activeClass: "ui-state-hover",
-            tolerance: "pointer",
-            over: function( event, ui ) {
-                $( ui.helper ).addClass( "ui-state-highlight" )
-            },
-            out: function( event, ui ) {
-                $( ui.helper ).removeClass( "ui-state-highlight" )
-            },
-            drop: function( event, ui ) {
-                $(ui.helper).remove();
-                
-            }
-        });
+        var add_element_item_widget_select;
+        var add_element_item_widget_text;
+        var trash_dash;
+        var item_widget_box_icons;
+        var select_dashicons_dashicons;
         
         refresh_d();
 
-        function refresh_d(destroy = false){
-            if(destroy){
-                $( ".select_dashicons .dashicons" ).draggable( "destroy" );
-                boxwidget_item_widget.draggable( "instance" );
-            }
-            $( ".select_dashicons .dashicons" ).draggable({
-                revert: true
+        function refresh_d(){
+
+            
+            select_dashicons_dashicons = $( ".select_dashicons .dashicons" ).draggable({
+                revert: true,
+                revertDuration: 100,
+                start: function( event, ui ) {
+                    $( ".item_widget .box_icons" ).addClass('box_icons-state-accept');
+                },
+                stop: function( event, ui ) {
+                    $( ".item_widget .box_icons" ).removeClass('box_icons-state-accept');
+                }
+
             });
-            $( ".item_widget .box_icons" ).droppable({
-                accept: $( ".select_dashicons .dashicons" ),
+            cont_widget = $( "#cont_widget" ).sortable({
+                revert: 100,
+                tolerance: "pointer",
+                placeholder: "ui-sortable-highlight",
+                axis: "x",
+                stop: function(){
+                    $( ".boxwidget:not(.ui-sortable-placeholder)" ).removeAttr('style');
+                }
+            });
+            boxwidget = $( ".boxwidget" ).sortable({
+                revert: 100,
+                tolerance: "pointer",
+                placeholder: "ui-sortable-highlight",
+                stop: function(){
+                    $( ".item_widget:not(.ui-sortable-placeholder)" ).removeAttr('style');
+                }
+            });
+            boxwidget.disableSelection();
+            adminmenu_a = $( "#adminmenu a" ).draggable({
+                connectToSortable: boxwidget,
+                helper: function(e) {
+                    var icon = 'dashicons-open-folder';
+                    //var classItem = $('.wp-menu-image',this).removeClass('wp-menu-image').removeClass('dashicons-before').attr('class');
+                    var classItem = $('.wp-menu-image',this).attr('class');
+                    if((classItem != undefined) && (!$('.wp-menu-image',this).hasClass('svg'))){
+                        icon = classItem;
+                    }
+                    
+                    return $('<div class="item_widget"><a href="'+$(this).attr('href')+'" ><span class="box_icons dashicons '+icon+'"></span>'+$(this).text()+'</a></div>');
+                },
+                revert: "invalid",
+                stop: function( event, ui ) {
+                    
+                    $( ".item_widget" ).draggable("instance");
+                    refresh_d();
+                    
+                },
+                start: function( event, ui ) {
+                    $('#submit').focus();
+                }
+                
+            });
+            
+            
+            add_element_item_widget = $( ".add_element .item_widget" ).draggable({
+                connectToSortable: boxwidget,
+                revert: 'invalid',
+                helper: 'clone',
+                tolerance: "pointer",
+                stop: function( event, ui ) {
+                    refresh_d();
+                }
+            });
+            add_element_item_widget_select = $( ".add_element .item_widget_select" ).draggable({
+                connectToSortable: boxwidget,
+                revert: 'invalid',
+                helper: function(e) {
+                    var type = $(this).attr('attr-type');
+                    var id = $('#id'+type+' option:selected' ).val();
+                    var title = $('#id'+type+' option:selected' ).text();
+                    return $('<div class="item_widget"><a href="http://localsite.ddns.net/test/wp-admin/post.php?post='+id+'&action=edit" ><span class="box_icons"></span>'+title+'</a></div></div>');
+                },
+                stop: function( event, ui ) {
+                    
+                    //$( ".item_widget:not(.ui-sortable-placeholder)" ).removeAttr('style');
+                    $( ".item_widget" ).draggable("instance");
+                    refresh_d();
+                },
+                tolerance: "pointer"
+            });
+            add_element_item_widget_text = $( ".add_element .item_widget_text" ).draggable({
+                connectToSortable: boxwidget,
+                revert: 'invalid',
+                helper: function(e) {
+                    var t = $('#input_widget_text').val();
+                    
+                    return $('<div class="item_widget">'+t+'</div>');
+                },
+                stop: function( event, ui ) {
+                    //$( ".item_widget:not(.ui-sortable-placeholder)" ).removeAttr('style');
+                    $( ".item_widget" ).draggable("instance");
+                    refresh_d();
+                },
+                tolerance: "pointer"
+            });
+            trash_dash = $( "#trash_dash" ).droppable({
+                accept: ".boxwidget .item_widget",
+                classes: {
+                    "ui-droppable-hover": "trash-state-active"
+                },
+                activeClass: "ui-state-hover",
+                tolerance: "pointer",
+                over: function( event, ui ) {
+                    $( ui.helper ).addClass( "ui-state-highlight" )
+                },
+                out: function( event, ui ) {
+                    $( ui.helper ).removeClass( "ui-state-highlight" )
+                },
+                drop: function( event, ui ) {
+                    $(ui.helper).remove();
+                }
+            });
+            item_widget_box_icons = $( ".item_widget .box_icons" ).droppable({
+                accept: select_dashicons_dashicons,
                 classes: {
                     "ui-droppable-hover": "box_icons-state-active"
                 },
                 helper: "clone",
                 revert: "invalid",
                 drop: function( event, ui ) {
-                    $(event.target).removeClass().addClass('dashicons').addClass(ui.helper.attr('atr-class'));
+                    $(event.target).removeClass().addClass('box_icons').addClass('dashicons').addClass(ui.helper.attr('atr-class'));
                     $( ".item_widget .dashicons" ).draggable("instance");
                     //$(ui.helper).remove();
                     
                 }
-            })
+            });
             var h_helper_boxwidget_item_widget = 0;
             var w_helper_boxwidget_item_widget = 0;
             boxwidget_item_widget = $( ".boxwidget .item_widget" ).draggable({
@@ -141,23 +166,19 @@ jQuery(function($){
 
         }
 
-        /*$('#cont_widget .boxwidget').on('DOMSubtreeModified', function(e){
-
-            console.log(this)
-            
-        });*/
-
 
         $('#form_dash').submit(function (e) { 
 
+            $('#cont_widget')
+                .removeClass('ui-sortable');
             $('#cont_widget *')
-            .removeClass('ui-sortable')
-            .removeClass('ui-sortable-handle')
-            .removeClass('ui-draggable')
-            .removeClass('ui-draggable-handle')
-            .removeClass('ui-droppable')
-            .removeClass('ui-droppable-handle')
-            .removeClass('svg');
+                .removeClass('ui-sortable')
+                .removeClass('ui-sortable-handle')
+                .removeClass('ui-draggable')
+                .removeClass('ui-draggable-handle')
+                .removeClass('ui-droppable')
+                .removeClass('ui-droppable-handle')
+                .removeClass('svg');
 
             var html = $.trim($('#cont_widget').html());
             //var obj_html = $.parseHTML($.trim($('#cont_widget').html()));
@@ -191,6 +212,7 @@ jQuery(function($){
             var v = $('#n_col').val();
             if($( ".boxwidget" ).length < v){
                 $('#cont_widget').append('<div class="boxwidget"></div>');
+                refresh_d();
             }else if($( ".boxwidget" ).length > v){
                 if ($('#cont_widget .boxwidget:last-child').is(':empty')) {
                     $('#cont_widget .boxwidget:last-child').remove();
@@ -208,7 +230,7 @@ jQuery(function($){
                     }
                 }
             }
-
+            boxwidget.sortable( "refresh" );
         });
 
 
