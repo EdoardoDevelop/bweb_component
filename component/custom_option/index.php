416abbbb9208fb8bc932bc141a8e71760b1b2dd3
edoardo.develop@gmail.com <edoardo.develop@gmail.com>
2023-02-10 08:36:36 +0100

    custom option field

---
+++ component/custom_option/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * ID: bc_custom_option_field
+ * Name: Campi Opzioni Personalizzati
+ * Description: Aggiunge campi opzioni personalizzati.
+ * Icon: dashicons-button
+ * 
+ */
+require plugin_dir_path( __FILE__ ) ."inc/settings.php";
+require plugin_dir_path( __FILE__ ) ."inc/_create.php";
