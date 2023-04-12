e740385bf4393ff840ccb9996b04db0d223fd873
edoardo.develop@gmail.com <edoardo.develop@gmail.com>
2023-04-12 06:18:32 +0200

    Update bweb_component.php

--- bweb_component.php
+++ bweb_component.php
@@ -4,7 +4,7 @@ Plugin Name: Bweb Component
 Plugin URI: https://github.com/EdoardoDevelop/bweb_component/
 Description: Plugin per Wordpress per lo sviluppo di siti web. Il plugin contiene componenti/script attivabili su necessità.
 Author: Edoardo Monti (Bweb)
-Version: 1.0.6
+Version: 1.0.7
 Author URI: https://www.bwebagency.it/
 */
 if (!defined("ABSPATH")) {
